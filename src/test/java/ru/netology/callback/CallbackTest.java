76042877f3c75b58be74f784807a941b625d9dd6
EvheniiV <jenyokgm@gmail.com>
2023-09-28 23:49:03 +0300

    fixed code 2

--- src/test/java/ru/netology/callback/CallbackTest.java
+++ src/test/java/ru/netology/callback/CallbackTest.java
@@ -3,6 +3,8 @@ package ru.netology.callback;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -11,11 +13,11 @@ public class CallbackTest {
     @Test
     void correctDataTest() {
         open("http://localhost:9999");
-        SelenideElement form = $("[class=\"form form_size_m form_theme_alfa-on-white\"]");
+        SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Василий");
         form.$("[data-test-id=phone] input").setValue("+79270000000");
         form.$("[data-test-id=agreement]").click();
-        form.$("[class=\"button button_view_extra button_size_m button_theme_alfa-on-white\"]").click();
+        form.$(".button").click();
         $(".paragraph").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
